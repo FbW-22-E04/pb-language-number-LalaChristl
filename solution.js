8a45688ecbf33f06db3519c88edaef46b7f49551
LalaChristl <lesleyannchristl@gmail.com>
2022-07-05 13:39:25 +0200

    added js

---
+++ solution.js
@@ -0,0 +1,15 @@
+// Question 1
+
+let introSentence = 'hello, my name is Fran and I am';
+let age = '25';
+console.log(introSentence, "" + age);
+
+// the + operator is adding the variables (including the number) and the 'space' to make a string
+
+// Question 2
+
+let integer = '1005';
+console.log('integer is', parseInt(integer));
+
+let float = '10.05';
+console.log('float is', parseFloat(float))
